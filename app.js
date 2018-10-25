d82e0de0684b90fa1f6ed18c9fe11e758bc769cc
lynnjepsen <lynnjepsen@google.com>
2018-10-25 16:08:36 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+import {MDCTopAppBar} from '@material/top-app-bar';
+import {MDCTabBar} from '@material/tab-bar';
+
+const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
+const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
+
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = [{
+  entry: ['./app.scss', './app.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              path: path.resolve(__dirname, 'dist'),
+              name: 'bundle.css',
+            },
+          },
+          { loader: 'extract-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'sass-loader',
+            options: {
+              includePaths: ['./node_modules']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+          plugins: ['transform-object-assign']
+        },
+      }
+    ]
+  },
+}];
