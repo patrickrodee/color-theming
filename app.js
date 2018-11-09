1892523ee1fc19870d297ae1496d65e33dd71b9b
lynnjepsen <lynnjepsen@google.com>
2018-11-09 14:53:34 -0800

    rename to adopt a pup, and add tabs

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ import {MDCTabBar} from '@material/tab-bar';
 const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
 const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
 
+tabBar.listen('MDCTabBar:activated', (activatedEvent) => {
+  document.querySelectorAll('.adopt-a-pup-body').forEach((element, index) => {
+    if (index === activatedEvent.detail.index) {
+      element.classList.remove('adopt-a-pup-body--hidden');
+    } else {
+      element.classList.add('adopt-a-pup-body--hidden');
+    }
+  });
+});
+
