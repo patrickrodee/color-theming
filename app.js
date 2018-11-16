ee060a2bfa43b8833323754e2ff3fd423325e16f
Glitch (adopt-a-pup) <none>
2018-11-16 19:40:03 +0000

    🌌😤 Checkpoint ./app.js:2247606/4473 ./index.html:2247606/34327 ./app.scss:2247606/3880 ./package.json:2247606/2571 ./states.json:2247606/4221

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 import {MDCTopAppBar} from '@material/top-app-bar';
 import {MDCTabBar} from '@material/tab-bar';
+import {MDCTextField} from '@material/textfield';
+import {MDCSelect} from '@material/select';
+import {MDCChipSet} from '@material/chips';
+import states from './states.json';
 
 const topAppBar = new MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
 const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
@@ -14,26 +18,82 @@ tabBar.listen('MDCTabBar:activated', (activatedEvent) => {
   });
 });
 
+/* form code */
+const selectElement = document.querySelector('.mdc-select');
+const textFields = document.querySelectorAll('.mdc-text-field');
+const adoptFormStateSelect = document.querySelector('.adopt-form__states');
+const chipSetEl = document.querySelector('.mdc-chip-set');
+
+textFields.forEach(tf => {
+  const textField = new MDCTextField(tf);
+});
+const mdcSelect = new MDCSelect(selectElement);
+
+
+const chipSet = new MDCChipSet(chipSetEl);
+
+const populateStates = () => {
+  setTimeout(() => {
+    states.forEach(state => {
+      const option = document.createElement('option');
+      option.classList.add('mdc-list-item');
+      option.setAttribute('data-value', state.abbreviation);
+      option.innerText = state.name;
+      adoptFormStateSelect.appendChild(option);
+    });
+  });
+}
+populateStates();
 
 let currentAdoptCardForm = null;
+const animatingClass = 'adopt-form__animating-container--animating';
+const animatingContainerClass = 'adopt-form__animating-container';
 const adoptForm = document.querySelector('.adopt-form');
-const cardActions = document.querySelectorAll('.mdc-card__primary-action');
+const adoptFormButtons = document.querySelectorAll('.adopt-form__button');
 
 const removeForm = (card) => {
-  
+  const animatingAdoptForm = card.querySelector(`.${animatingContainerClass}`);
+  const cardActions = card.querySelector('.mdc-card__actions');
+  cardActions.querySelector('.adopt-form__button-text').innerText = 'Adopt Me!';
+  cardActions.querySelector('.adopt-form__button-icon').innerText = 'pets';
+
+  animatingAdoptForm.classList.add(animatingClass);
+  setTimeout(() => {
+    if (!card) return;
+    card.removeChild(adoptForm);
+    document.body.appendChild(adoptForm);
+  }, 200);
 }
 
-const handleCardActionClick = (e) => {
+const addForm = (card) => {
+  const cardActions = card.querySelector('.mdc-card__actions');
+  card.insertBefore(adoptForm, cardActions);
+  cardActions.querySelector('.adopt-form__button-text').innerText = 'Send Info';
+  cardActions.querySelector('.adopt-form__button-icon').innerText = 'send';
+  const animatingAdoptForm = card.querySelector(`.${animatingContainerClass}`);
+
+  animatingAdoptForm.classList.add(animatingClass);
+  requestAnimationFrame(() => {
+    animatingAdoptForm.classList.remove(animatingClass);
+  });
+}
+
+const handleAdoptFormClick = (e) => {
+  // not supported by IE11
+  const card = e.target.closest('.mdc-card');
+
   if (currentAdoptCardForm) {
     removeForm(currentAdoptCardForm);
-    if (currentAdoptCardForm === e.target) return;
+    if (currentAdoptCardForm === card) {
+      currentAdoptCardForm = null;
+      return;
+    };
   }
-  
-  currentAdoptCardForm = e.target;
-  
-  
+
+  currentAdoptCardForm = card;
+  addForm(currentAdoptCardForm);
 }
 
-cardActions.forEach(cardAction => {
-  document.addEventListener('click', handleCardActionClick);
+adoptFormButtons.forEach(adoptFormButton => {
+  adoptFormButton.addEventListener('click', handleAdoptFormClick);
 });
