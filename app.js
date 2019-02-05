0b0684fa5fbeb51f3b427277736c02f62d291369
Glitch (material-starter-kit) <none>
2019-02-05 16:00:26 +0000

    🌃🍟 Checkpoint ./app.js:201274/479 ./app.scss:201274/31

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 import {MDCRipple} from '@material/ripple/index';
 
-const ripple = new MDCRipple(document.querySelector('.foo-button'));
+const buttons = [...document.querySelectorAll('.mdc-button')];
+const ripples = new Set();
+
+buttons.forEach((btn) => {
+  ripples.add(new MDCRipple(btn));
+  let button = new MDCRipple(btn);
+  console.log(button);
+});
+
+// const ripple = new MDCRipple(document.querySelector('.mdc-button'));
