3d2f589bf3a19a7b6c17012a8a52c6e9a3e79525
Glitch (material-starter-kit) <none>
2019-02-05 16:11:25 +0000

    ☠️💀 Checkpoint ./app.js:201274/73 ./button/_theme.scss:201274/281

--- app.js
+++ app.js
@@ -5,8 +5,4 @@ const ripples = new Set();
 
 buttons.forEach((btn) => {
   ripples.add(new MDCRipple(btn));
-  let button = new MDCRipple(btn);
-  console.log(button);
 });
-
-// const ripple = new MDCRipple(document.querySelector('.mdc-button'));
