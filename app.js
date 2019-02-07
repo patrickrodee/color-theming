1d04f545ce2c9a437aaa493427ff85c4bc2bd6bd
Glitch (material-starter-kit) <none>
2019-02-07 00:09:56 +0000

    📕🎤 Checkpoint ./index.html:201274/566 ./app.js:201274/449 ./chips/_theme.scss:201274/137 ./colors/_gm_theme.scss:201274/208 ./chips/_main.scss:201274/45

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 import {MDCRipple} from '@material/ripple/index';
 import {MDCTextField} from '@material/textfield/index';
+import {MDCChipSet} from '@material/chips';
 
 const buttons = new Set();
-[...document.querySelectorAll('.mdc-button')].forEach((btn) => {
-  buttons.add(new MDCRipple(btn));
+[...document.querySelectorAll('.mdc-button')].forEach((button) => {
+  buttons.add(new MDCRipple(button));
 });
 
 const textfields = new Set();
-[...document.querySelectorAll('.mdc-text-field')].forEach((tf) => {
-  textfields.add(new MDCTextField(tf));
+[...document.querySelectorAll('.mdc-text-field')].forEach((textfield) => {
+  textfields.add(new MDCTextField(textfield));
+});
+
+const chipsets = new Set();
+[...document.querySelectorAll('.mdc-chip-set')].forEach((chipset) => {
+  chipsets.add(new MDCChipSet(chipset));
 });
